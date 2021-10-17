5b43e079e18dd911d3452cc8b3a9003e8006b46a
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-10-17 15:01:51 +0200

    Add Enhanced Login Page plug-in

--- src/desktop/index.js
+++ src/desktop/index.js
@@ -1,6 +1,7 @@
 import canvas from '@artevelde-uas/canvas-lms-app';
 
 import enableCourseRecycleBinPlugin from '@artevelde-uas/canvas-lms-enable-course-recycle-bin-plugin';
+import enhancedLoginPagePlugin from '../../packages/plugins/enhanced-login-page/src/';
 import exportCourseGroupsPlugin from '@artevelde-uas/canvas-lms-export-course-groups-plugin';
 import fileExplorerEnhancementsPlugin from '@artevelde-uas/canvas-lms-file-explorer-enhancements-plugin';
 import fixFilesNavigationPlugin from '@artevelde-uas/canvas-lms-fix-files-navigation-plugin';
@@ -9,6 +10,12 @@ import './index.css';
 
 
 canvas.addPlugin(enableCourseRecycleBinPlugin);
+canvas.addPlugin(enhancedLoginPagePlugin, {
+    logo: require('../../assets/canvasahs_white.svg'),
+    studentStaffImage: require('../../assets/students.jpg'),
+    guestImage: require('../../assets/external.jpg'),
+    studentStaffLoginUrl: 'microsoft/5'
+});
 canvas.addPlugin(exportCourseGroupsPlugin);
 canvas.addPlugin(fileExplorerEnhancementsPlugin);
 canvas.addPlugin(fixFilesNavigationPlugin);
