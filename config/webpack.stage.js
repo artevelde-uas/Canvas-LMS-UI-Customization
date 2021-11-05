3a0d21537f20d327f29d33d5789b91689eea90e4
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-11-05 16:46:46 +0100

    fix: Exclude `node_modules` from build

--- config/webpack.stage.js
+++ config/webpack.stage.js
@@ -28,6 +28,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.jsx?$/,
+            exclude: /node_modules/,
             use: [{
                 loader: 'babel-loader',
                 options: {
