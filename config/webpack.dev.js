58a20b1b257e92c146aae9d458eabbbb72f534ab
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-17 16:22:20 +0100

    Better source maps

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,6 +3,7 @@ const developmentConfig = require('@artevelde-uas/canvas-lms-app/webpack/develop
 
 module.exports = {
     ...developmentConfig,
+    devtool: 'eval-source-map',
     entry: {
         desktop: [
             './src/hide-test-env-warning.css',
